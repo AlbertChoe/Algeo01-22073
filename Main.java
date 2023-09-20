f28daa0c7fd2985191e313ec6c17914eaae8dc81
Juan A <140235896+juanaw6@users.noreply.github.com>
2023-09-21 03:00:16 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("test");
         //Get nRow + nCol
         System.out.print("Enter the number of rows: ");
         int numRows = scanner.nextInt();
