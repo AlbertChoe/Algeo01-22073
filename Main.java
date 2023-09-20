a026597e0c2da36552f99f1896ad16b91a31a3c6
Juan A <140235896+juanaw6@users.noreply.github.com>
2023-09-21 03:07:39 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("TEST!");
+        System.out.println("TEST branch");
         //Get nRow + nCol
         System.out.print("Enter the number of rows: ");
         int numRows = scanner.nextInt();
